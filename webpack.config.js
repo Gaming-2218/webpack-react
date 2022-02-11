b750b2fb0f901d962c9ea230d0ab66ec7f390ef8
Erikh22l18 <e_2019100825C@uncp.edu.pe>
2022-02-10 21:58:38 -0500

    updated files

--- webpack.config.js
+++ webpack.config.js
@@ -52,7 +52,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './public/index.html',
-      filename: './index.html'
+      filename: 'index.html'
     }),
     new MiniCssExtractPlugin({
       filename: '[name].css'
